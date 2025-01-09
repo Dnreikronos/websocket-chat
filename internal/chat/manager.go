6e4069aa90193f07367adac9ddd6d3f0ebc03735
Dnreikronos <dnreikronos2@gmail.com>
2025-01-08 20:06:55 -0400

    feat: create run function

--- internal/chat/manager.go
+++ internal/chat/manager.go
@@ -15,3 +15,21 @@ func NewManager() *Manager {
 		Unregister: make(chan *Client),
 	}
 }
+
+func (m *Manager) Run() {
+	for {
+		select {
+		case client := <-m.Register:
+			m.Clients[client.ID] = client
+		case client := <-m.Unregister:
+			if _, ok := m.Clients[client.ID]; ok {
+				delete(m.Clients, client.ID)
+				close(client.Send)
+			}
+		case msg := <-m.Broadcast:
+			if client, ok := m.Clients[msg.ReceiverID]; ok {
+				client.Send <- msg
+			}
+		}
+	}
+}
